0e23681252067279f29ce76ed7eefc7e91adff78
Saurabh Srivastava <me@saurabhsrivastava.dev>
2021-10-15 02:14:17 +0530

    Exporting a class from module

--- cppbits.test/cppbits.test.cpp
+++ cppbits.test/cppbits.test.cpp
@@ -1,21 +1,24 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include <string>
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 import PhoneKeypadLetterCombinations;
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-using namespace cppbits::algos;
-
 namespace cppbitstest
 {
-	TEST_CLASS(cppbitstest)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			Assert::IsTrue(GetNumberGreaterThanZero() > 0);
-		}
-	};
+    TEST_CLASS(cppbitstest)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            cppbits::algos::PhoneKeypadLetterCombinations pk;
+            vector<string> res = pk.letterCombinations("2");
+            Assert::AreEqual(3, (int)res.size());
+        }
+    };
 }
